14673ac6c90cdd1faa192b88d82f8524e705ebc7
abodie <abodi.o.alnooh@gmail.com>
2015-06-14 21:08:42 +0300

    appstoreiraq
    dev by abdullah
    template
    http://www.idangero.us/framework7

---
+++ appstoreiraq/index.php
@@ -0,0 +1,188 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<title>
+	 appstoreiraq
+	</title>
+	<link rel="stylesheet" href="css/framework7.min.css">
+	<link rel="stylesheet" href="css/my-app.css">
+	<link rel="stylesheet" href="css/appstoreiraq.css">
+  </head>
+  <body>
+	<div class="statusbar-overlay">
+	</div>
+	<div class="panel-overlay">
+	</div>
+
+	<!-- Left panel with reveal effect-->	
+	<div class="panel panel-left panel-reveal">
+	</div>
+	<div class="views tabs toolbar-through">
+	  <div id="view-1" class="view view-main tab active" data-page="index-1">
+		<div class="navbar">
+		  <div class="navbar-inner">
+			<div class="left">
+			  <a href="#" data-panel="left" class="open-panel"></a>
+			</div>
+			<div class="center sliding">
+			Appstore Iraq
+			</div>
+			<div class="right">
+			  <a href="" onClick="location.reload();" class="link">Reload</a>
+			</div>
+		  </div>
+		</div>
+		<div class="pages navbar-through">
+		  <div data-page="index-1" class="page">
+			<div class="page-content">
+			<div>
+			
+				<div class="row">
+			  <div class="col-auto tablet-auto"></div>
+				<div class="col-95 tablet-66">
+				  <div class="buttons-row" style="margin-top: 5px">
+					<a href="https://www.facebook.com/moshax.baghdady" class="external button button-round">facebook</a>
+					<a href="https://www.paypal.com/ca/cgi-bin/webscr?cmd=_flow&SESSION=Z_U0dqzk7yPcr47ZVtn73W1WfQqc_iuwze4JnBdOYvEAp6BJAMfNX_FoS0q&dispatch=5885d80a13c0db1f8e263663d3faee8d99e4111b56ef0eae45e68b8988f5b2dd" class="external button button-round">Donate</a>
+					<a href="twitter://user?screen_name=abodie984" class="external button button-round">Twitter</a>
+				  </div>
+				</div>
+			 <div class="col-auto tablet-auto"></div>
+			  </div>
+			  </div>
+
+
+<center>
+<div class="des">
+<b>ابستور العراق</b><br><b>لتحميل تطبيقات الايفون المحذوفة من الابستور مجانا وبدون جلبريك </b><br>
+<b>يمكنك دعمي على باي بال </b><br>
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHPwYJKoZIhvcNAQcEoIIHMDCCBywCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYC8lB68flUE4KMfKjR0FMSJOxCALcJhJRfoxtH7Tn+QepoS7HkWLyQRU0nz4VZoKV2wTP/azvjmF4Mi1DorCoOWUof/4soKEL4vXQ9SjFFQHA7KMGXOqIBism4DLIxNJ2ux0X2Tn1Ur2AUyAWraNQy4iWRFIADSPmu29PkJ/2mF6zELMAkGBSsOAwIaBQAwgbwGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIp/k9OYiM82WAgZiHQVY3WGevhStUcPSPgu7pqWdGbcCZ7eXvaivj63FaGaHIoShNryaJVFcqFUqlXbZGeGufjNP1+fGQwCnOTRI2qN9DLcaoHZwM2f1XZK+d4R+kAKyiStfxaShITcbHZ0ujcUU6lLL59hDc5wsML4FbUdk6bKBkd+QgPoFEDHHTzaIpCHXQbBlOEhwEXdRKFKR2aGWQVkt/mKCCA4cwggODMIIC7KADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTAeFw0wNDAyMTMxMDEzMTVaFw0zNTAyMTMxMDEzMTVaMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwUdO3fxEzEtcnI7ZKZL412XvZPugoni7i7D7prCe0AtaHTc97CYgm7NsAtJyxNLixmhLV8pyIEaiHXWAh8fPKW+R017+EmXrr9EaquPmsVvTywAAE1PMNOKqo2kl4Gxiz9zZqIajOm1fZGWcGS0f5JQ2kBqNbvbg2/Za+GJ/qwUCAwEAAaOB7jCB6zAdBgNVHQ4EFgQUlp98u8ZvF71ZP1LXChvsENZklGswgbsGA1UdIwSBszCBsIAUlp98u8ZvF71ZP1LXChvsENZklGuhgZSkgZEwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAgV86VpqAWuXvX6Oro4qJ1tYVIT5DgWpE692Ag422H7yRIr/9j/iKG4Thia/Oflx4TdL+IFJBAyPK9v6zZNZtBgPBynXb048hsP16l2vi0k5Q2JKiPDsEfBhGI+HnxLXEaUWAcVfCsQFvd2A1sxRr67ip5y2wwBelUecP3AjJ+YcxggGaMIIBlgIBATCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTE1MDYxNDA5MDQxMFowIwYJKoZIhvcNAQkEMRYEFLHwiMmXkCcav/TthqJr8lpX6UlcMA0GCSqGSIb3DQEBAQUABIGAG05oNuG9d1eOWLu6uAsuIrgFbJU/Tp9eM4QAqVIm2b0G0idrkWAM7Una+aQEOtO/obwIdzzcugB3U6QtyIxPEloXGSic59l946ri1elxy96M8Y6/Znz+44dN7d7ZeQPCOFV09dOYagJh0BKyVe1bUHUFxdI2ezEhZvf5aznomjM=-----END PKCS7-----
+">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+				
+			</div>
+
+
+</center>
+
+ <div class="list-block media-list">
+
+   <ul>
+
+    <li>
+      <a href="itms-services://?action=download-manifest&url=https://archive.org/download/playboxhd_20150610/playboxhd.plist" class="item-link item-content">
+        <div class="item-media"><img src="http://i.utdstc.com/icons/120/playbox-android.png" width="44" style="border-radius: 15.625%;"></div>
+        <div class="item-inner">
+          <div class="item-title-row">
+            <div class="item-title" style="z-index:10;">PlayBoxHD</div>
+          </div><div class="item-subtitle">By abdullah omar</div>
+        </div>
+      </a>
+    </li>
+
+    <li>
+      <a href="itms-services://?action=download-manifest&url=https://archive.org/download/abodioalnooh_gmail_Shou/Shou.plist" class="item-link item-content">
+        <div class="item-media"><img src="https://shoutvassets0.cachefly.net/assets/shou-aede3e89b8530d7732f5b0e7220b6daf.png" width="44" style="border-radius: 15.625%;" ></div>
+        <div class="item-inner">
+          <div class="item-title-row">
+            <div class="item-title" style="z-index:10;">shou</div>
+          </div><div class="item-subtitle">By abdullah omar</div>
+        </div>
+      </a>
+    </li>
+
+    <li>
+      <a href="itms-services://?action=download-manifest&url=https://archive.org/download/ppsspp_201506/ppsspp.plist" class="item-link item-content">
+        <div class="item-media"><img src="pic/psp.png" width="44" style="border-radius: 15.625%;" ></div>
+        <div class="item-inner">
+          <div class="item-title-row">
+            <div class="item-title" style="z-index:10;">ppsspp</div>
+          </div><div class="item-subtitle">By abdullah omar</div>
+        </div>
+      </a>
+    </li>
+
+   <li>
+      <a href="itms-services://?action=download-manifest&url=https://archive.org/download/musicbox_201506/musicbox.plist" class="item-link item-content">
+        <div class="item-media"><img src="pic/mub.png" width="44" style="border-radius: 15.625%;" ></div>
+        <div class="item-inner">
+          <div class="item-title-row">
+            <div class="item-title" style="z-index:10;">musicbox</div>
+          </div><div class="item-subtitle">By abdullah omar</div>
+        </div>
+      </a>
+    </li>
+
+       <li>
+      <a href="itms-services://?action=download-manifest&url=https://archive.org/download/moviebox_20150610/moviebox.plist" class="item-link item-content">
+        <div class="item-media"><img src="pic/movie.png" width="44" style="border-radius: 15.625%;" ></div>
+        <div class="item-inner">
+          <div class="item-title-row">
+            <div class="item-title" style="z-index:10;">MovieBox</div>
+          </div><div class="item-subtitle">By abdullah omar</div>
+        </div>
+      </a>
+    </li>
+
+      <li>
+      <a href="itms-services://?action=download-manifest&url=https://archive.org/download/videoDownloadpro_201506/videoDownloadpro.plist" class="item-link item-content">
+        <div class="item-media"><img src="pic/video.png" width="44" style="border-radius: 15.625%;" ></div>
+        <div class="item-inner">
+          <div class="item-title-row">
+            <div class="item-title" style="z-index:10;">video downloader</div>
+          </div><div class="item-subtitle">By abdullah omar</div>
+        </div>
+      </a>
+    </li>
+
+  </ul>
+
+
+ </ul>
+
+
+
+
+
+</div>
+
+
+
+
+<style type="text/css">
+.card{font-size: 30px;}
+i{
+	font-size: 25px;
+}
+</style>
+		
+		</div>
+	  </div>
+	</div>
+  </div>
+
+	<!-- Bottom Tabbar  
+	<div class="toolbar tabbar tabbar-labels">
+	  <div class="toolbar-inner">
+		<a href="manage.php" class="tab-link active">
+	<span class="tabbar-label" > <i class="fa fa-home"><b>Home</b></i></span></a>
+		<a href="manage.php" id="manage-button" class="tab-link"><span class="tabbar-label"><i class="fa fa-home">APPS</l></span>
+		</a>
+	  </div>
+	</div>
+  </div>
+</div>  
+-->	
+
+
+</body>
+</html>
